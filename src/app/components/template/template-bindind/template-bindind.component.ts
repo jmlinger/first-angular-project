3c18dbbcec1c16fe1f3973e92001ac12f9418ef8
jmlinger <jmlinger10@gmail.com>
2024-01-10 18:03:06 -0300

    feat:continuing to explore bindings

--- src/app/components/template/template-bindind/template-bindind.component.ts
+++ src/app/components/template/template-bindind/template-bindind.component.ts
@@ -14,13 +14,31 @@ export class TemplateBindindComponent {
   public condition = this.age > 1 ? 'test' : 'test2'
   public isDisabled = true
   public srcValue = 'https://digimon.shadowsmith.com/img/myotismon.jpg'
-  public sum (parm1: number, parm2: number) {
-    return parm1 + parm2
+  public isTextDecoration = this.age >= 32 ? 'underline' : 'none'
+  public sum () {
+    return this.age++
+  }
+
+  public sub () {
+    return this.age--
   }
 
   constructor() {
     setTimeout(() => {
       this.name = "João e Maria"
     }, 2000);
+  }
+
+  public onKeyDown(event: Event){
+    return console.log(event);
+
+  }
+
+  public mousePositionX = 0
+  public mousePositionY = 0
+
+  public onMouseMove(event: MouseEvent){
+    this.mousePositionX = event.clientX
+    this.mousePositionY = event.clientY
   }
 }
